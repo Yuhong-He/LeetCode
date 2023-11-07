b85ea56b1230e656250fd32ce6448fba8bb2a3fa
Yuhong He <71640320+Theodore-Ho@users.noreply.github.com>
2023-11-07 13:31:19 +0000

    MedianOfTwoSortedArrays

---
+++ Array/src/MedianOfTwoSortedArrays.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MedianOfTwoSortedArrays {
+    public static void main(String[] args) {
+        int[] nums1 = new int[] {1, 3};
+        int[] nums2 = new int[] {3, 4};
+        double expected = 2.5;
+        double actual = findMedianSortedArrays(nums1, nums2);
+        System.out.println("expected: " + expected);
+        System.out.println("actual: " + actual);
+    }
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        List<Integer> list1 = Arrays.stream(nums1).boxed().collect(Collectors.toList());
+        List<Integer> list2 = Arrays.stream(nums2).boxed().collect(Collectors.toList());
+        list1.addAll(list2);
+        int[] nums = list1.stream().mapToInt(Integer::valueOf).toArray();
+        Arrays.sort(nums);
+        if (nums.length % 2 == 0) {
+            return (nums[nums.length / 2 - 1] + nums[nums.length / 2]) / 2.0;
+        } else {
+            return nums[Math.abs(nums.length / 2)];
+        }
+    }
+}
