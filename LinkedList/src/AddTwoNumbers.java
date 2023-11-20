3c1bc43d42a406a72bc2379603e8140047ba2118
Yuhong He <71640320+Theodore-Ho@users.noreply.github.com>
2023-11-20 11:23:21 +0000

    RemoveNthNodeFromEndOfList

--- LinkedList/src/AddTwoNumbers.java
+++ LinkedList/src/AddTwoNumbers.java
@@ -1,11 +1,4 @@
 public class AddTwoNumbers {
-    public static class ListNode {
-        int val;
-        ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-    }
 
     public static void main(String[] args) {
         ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
---
+++ LinkedList/src/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,37 @@
+public class RemoveNthNodeFromEndOfList {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        ListNode result = removeNthFromEnd(head, 2);
+        while (result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
+    }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        int length = getLength(head);
+        int target = length - n;
+        ListNode cur = dummy;
+        int index = 0;
+        while (cur != null) {
+            if (index == target) {
+                cur.next = cur.next.next;
+                break;
+            } else {
+                cur = cur.next;
+                index++;
+            }
+        }
+        return dummy.next;
+    }
+
+    public static int getLength(ListNode head) {
+        int length = 0;
+        while (head != null) {
+            length++;
+            head = head.next;
+        }
+        return length;
+    }
+}
