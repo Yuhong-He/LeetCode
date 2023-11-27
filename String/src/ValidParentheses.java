442876279f23c3ea335e462166f1ef94623e7699
Yuhong He <71640320+Theodore-Ho@users.noreply.github.com>
2023-11-27 11:22:48 +0000

    ValidParentheses

---
+++ String/src/ValidParentheses.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        String input = "){";
+        boolean expected = false;
+        boolean actual = isValid(input);
+        System.out.println("expected: " + expected);
+        System.out.println("actual: " + actual);
+    }
+
+    public static boolean isValid(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        Map<Character, Character> pairs = Map.of(
+                ')', '(',
+                ']', '[',
+                '}', '{'
+        );
+        Deque<Character> stack = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (pairs.containsKey(ch)) {
+                if (stack.isEmpty() || stack.peek() != pairs.get(ch)) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static boolean isValid2(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        Map<Character, Character> map = Map.of(
+                ')', '(',
+                ']', '[',
+                '}', '{'
+        );
+        List<Character> list = List.of('(', '[', '{');
+        List<Character> queue = new ArrayList<>();
+        int order1 = 0;
+        int order2 = 0;
+        for (int i = 0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            if (list.contains(c)) {
+                order1++;
+                queue.add(c);
+            } else {
+                order2++;
+                if (queue.isEmpty()) {
+                    return false;
+                }
+                if (queue.get(queue.size() - 1) == map.get(c)) {
+                    queue.remove(queue.size() - 1);
+                } else {
+                    return false;
+                }
+            }
+        }
+        return order1 == order2;
+    }
+}
