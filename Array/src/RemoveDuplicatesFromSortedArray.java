addb6d21d3b3144c65ac746c64558d0490635e8d
Yuhong He <71640320+Theodore-Ho@users.noreply.github.com>
2023-11-29 22:53:53 +0000

    RemoveDuplicatesFromSortedArray

---
+++ Array/src/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+        int[] input = {0,0,1,1,1,2,2,3,3,4};
+        int expected = 5;
+        int actual = removeDuplicates(input);
+        System.out.println("expected: " + expected);
+        System.out.println("actual: " + actual);
+        System.out.println(Arrays.toString(input));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                nums[count++] = nums[i];
+            }
+        }
+        return count;
+    }
+}
