2ba4c6de341123583cda0190ac77d0905f25371b
Yuhong He <71640320+Theodore-Ho@users.noreply.github.com>
2023-12-01 22:49:03 +0000

    RemoveElements

---
+++ Array/src/RemoveElements.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class RemoveElements {
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,2,3,0,4,2};
+        int val = 2;
+        int expected = 5;
+        int actual = removeElement(arr, val);
+        System.out.println("expected: " + expected);
+        System.out.println("actual: " + actual);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            if (nums[left] == val) {
+                nums[left] = nums[right - 1];
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return left;
+    }
+}
