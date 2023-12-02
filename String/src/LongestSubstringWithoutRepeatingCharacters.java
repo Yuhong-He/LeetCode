815fde630af4e9d23223766d2d9a4a18b9f08089
Yuhong He <71640320+Theodore-Ho@users.noreply.github.com>
2023-12-02 23:39:35 +0000

    some update

--- String/src/LongestSubstringWithoutRepeatingCharacters.java
+++ String/src/LongestSubstringWithoutRepeatingCharacters.java
@@ -15,20 +15,13 @@ public class LongestSubstringWithoutRepeatingCharacters {
         int leftIndex = -1;
         Set<Character> set = new HashSet<>();
         for (int i = 0; i < s.length(); i++) {
-//            System.out.println("i: " + i);
             if (i != 0) {
-//                System.out.println("s.charAt(i - 1): " + s.charAt(i - 1));
                 set.remove(s.charAt(i - 1));
             }
             while (leftIndex + 1 < s.length() && !set.contains(s.charAt(leftIndex + 1))) {
                 set.add(s.charAt(leftIndex + 1));
-//                System.out.println("s.charAt(leftIndex + 1): " + s.charAt(leftIndex + 1));
                 leftIndex++;
             }
-//            for (char n: set) {
-//                System.out.print(n + " ");
-//            }
-//            System.out.println(" ");
             longest = Math.max(longest, leftIndex - i + 1);
         }
         return longest;
