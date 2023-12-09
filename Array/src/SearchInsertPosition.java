b23e7fb265326096e992e2a55f0dd49972cfe28e
Yuhong He <71640320+Theodore-Ho@users.noreply.github.com>
2023-12-09 17:00:48 +0000

    SearchInsertPosition

---
+++ Array/src/SearchInsertPosition.java
@@ -0,0 +1,35 @@
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,6};
+        int target = 5;
+        int expected = 2;
+        int actual = searchInsert(nums, target);
+        System.out.println("expected: " + expected);
+        System.out.println("actual: " + actual);
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    public static int searchInsert2(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target || nums[i] > target) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
