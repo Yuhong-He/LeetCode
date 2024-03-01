ed61b710e982e7cb223d4237112def6c3acba491
Yuhong He <71640320+Theodore-Ho@users.noreply.github.com>
2024-03-01 09:51:07 +0000

    ValidParentheses

--- String/src/_20_ValidParentheses.java
+++ String/src/_20_ValidParentheses.java
@@ -10,6 +10,25 @@ public class _20_ValidParentheses {
     }
 
     public static boolean isValid(String s) {
+        if (s.length() <= 1) {
+            return false;
+        }
+        LinkedList<Character> stack = new LinkedList<>();
+        for (char c: s.toCharArray()) {
+            if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (c == '(') {
+                stack.push(')');
+            } else if (stack.isEmpty() || c != stack.pop()) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static boolean isValid2(String s) {
         if (s.length() % 2 != 0) {
             return false;
         }
@@ -33,7 +52,7 @@ public class _20_ValidParentheses {
         return stack.isEmpty();
     }
 
-    public static boolean isValid2(String s) {
+    public static boolean isValid3(String s) {
         if (s.length() % 2 != 0) {
             return false;
         }
